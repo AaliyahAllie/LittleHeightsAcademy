23ac7bc9395cbc796756de06fd087c6ee84171e3
Amaan Williams <129081794+ST10396397@users.noreply.github.com>
2025-10-20 19:35:46 +0200

    Admin message,marks,account,seats,view applications and view student account created

---
+++ app/src/main/java/com/example/littleheightsacademy/AdminUpdateSeatsActivity.kt
@@ -0,0 +1,20 @@
+package com.example.littleheightsacademy
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class AdminUpdateSeatsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_admin_update_seats)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
